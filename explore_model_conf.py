421b4c11c8aab1c56222de7af890aedfc21fbad8
nadbag98 <nadav.gat@gmail.com>
2023-06-18 17:08:10 +0300

    fix eval bug, tidy file up

--- explore_model_conf.py
+++ explore_model_conf.py
@@ -1,10 +1,12 @@
 import torchvision
-from torchvision.models import resnet50
+from torchvision.models import resnet50, googlenet
+# from torchvision.models import convnext_tiny, ConvNeXt_Tiny_Weights
 import torch
 from breaching.cases import construct_dataloader
 import hydra
 from omegaconf import OmegaConf
-from breaching.cases.data.datasets_vision import _get_meanstd, _parse_data_augmentations
+from breaching.cases.data.datasets_vision import _build_dataset_vision
+from breaching.cases.data.data_preparation import construct_dataloader
 import pickle
 
 device = torch.device(f"cuda:0")
@@ -13,58 +15,40 @@ device = torch.device(f"cuda:0")
 @hydra.main(config_path="breaching/config", config_name="my_cfg", version_base="1.1")
 def main_launcher(cfg):
     _default_t = torchvision.transforms.ToTensor()
-    model_name = "densenet121"
+    cfg.case.data.partition = "unique-class"
+    cfg.case.data.batch_size = 10
+    cfg.case.user.num_data_points = 10
+    cfg.case.model = "resnet50"
     ds_name = "imagenet"
     
-    model = getattr(torchvision.models, model_name)(pretrained=True)
-    
+    model = getattr(torchvision.models, cfg.case.model)(pretrained=True)
+    model.eval()
     model.to(device)
-    dataset = torchvision.datasets.ImageNet(
-            root=cfg.case.data.path, split="val" , transform=_default_t,
-        )
-    dataset.lookup = dict(zip(list(range(len(dataset))), [label for (_, label) in dataset.samples]))
 
-    if cfg.case.data.mean is None and cfg.data.normalize:
-        data_mean, data_std = _get_meanstd(dataset)
-        cfg.case.data.mean = data_mean
-        cfg.case.data.std = data_std
-
-    transforms = _parse_data_augmentations(cfg.case.data, "val")
-
-    # Apply transformations
-    dataset.transform = transforms if transforms is not None else None
-
-    # Save data mean and data std for easy access:
-    if cfg.case.data.normalize:
-        dataset.mean = cfg.case.data.mean
-        dataset.std = cfg.case.data.std
-    else:
-        dataset.mean = [0]
-        dataset.std = [1]
-
-    # print(len(dataset))
     avg_confs = {}
     std_confs = {}
-    samples_per_class = 64
+
     for ind in range(1000):
-        indices = [idx for (idx, label) in dataset.lookup.items() if label == ind][:samples_per_class]
-        indices = torch.tensor(indices)
-        inputs = [dataset[i][0] for i in indices]
-        inputs = torch.stack(inputs)
+        dataloader = construct_dataloader(cfg.case.data, cfg.case.impl, user_idx=ind, return_full_dataset=False)
+        for _, data_block in enumerate(dataloader):
+            inputs, labels = data_block["inputs"], data_block["labels"]
+            break
+
         inputs = inputs.to(device)
         outputs = model(inputs)
         outputs = outputs.softmax(dim=1).cpu()
+
         ind_avg_conf = outputs[:, ind].mean()
         ind_std_conf = outputs[:, ind].std()
+        
         avg_confs[ind] = ind_avg_conf.item()
         std_confs[ind] = ind_std_conf.item()
         print(f"{ind}: {ind_avg_conf.item()} +- {ind_std_conf.item()}")
     
-    # save avg_confs to a file named "vgg19_imagenet_avg_confs.pkl"
-    with open(f"{model_name}_{ds_name}_avg_confs.pkl", "wb") as f:
+    with open(f"{cfg.case.model}_{ds_name}_avg_confs.pkl", "wb") as f:
         pickle.dump(avg_confs, f)
        
-    with open(f"{model_name}_{ds_name}_std_confs.pkl", "wb") as f:
+    with open(f"{cfg.case.model}_{ds_name}_std_confs.pkl", "wb") as f:
         pickle.dump(std_confs, f)
 
 
